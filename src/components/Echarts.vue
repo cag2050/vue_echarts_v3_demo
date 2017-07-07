be47d2d1acaeab559abea74fdc9ac07843c82448
cag2050 <cag2050@163.com>
2017-07-07 19:33:19 +0800

    m

--- src/components/Echarts.vue
+++ src/components/Echarts.vue
@@ -1,15 +1,45 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <IEcharts :option="scoreOptions" style="width: 600px; height:400px"></IEcharts>
   </div>
 </template>
 
 <script>
+import IEcharts from 'vue-echarts-v3/src/full.vue'
+// import IEcharts from 'vue-echarts-v3/src/lite.vue'
+
 export default {
     name: 'hello',
+    components: {
+        IEcharts
+    },
     data () {
         return {
-            msg: 'echarts to Your Vue.js App'
+            msg: 'echarts to Your Vue.js App',
+            tooltip: {
+                formatter: '{a} <br/>{b} : {c}%'
+            },
+            series: [
+                {
+                    name: '业务指标',
+                    type: 'gauge',
+                    detail: {formatter: '{value}%'},
+                    data: [{value: 50, name: '完成率'}]
+                }
+            ]
+        }
+    },
+    computed: {
+        scoreOptions () {
+            console.log(this)
+            console.log(this.chartOptions)
+
+            let options = this.chartOptions
+            console.log(options)
+            options.tooltip = this.tooltip
+            options.series = this.series
+            return options
         }
     }
 }
