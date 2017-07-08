0e867c3c3a660e33e2d4dab87cebfbd9410d7141
cag2050 <cag2050@163.com>
2017-07-08 09:44:59 +0800

    m

--- src/components/Echarts.vue
+++ src/components/Echarts.vue
@@ -1,52 +1,48 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <IEcharts :option="scoreOptions" style="width: 600px; height:400px"></IEcharts>
+    <IEcharts ref='echart' :option="options" style="width: 600px; height:400px"></IEcharts>
   </div>
 </template>
 
 <script>
-import IEcharts from 'vue-echarts-v3/src/full.vue'
-// import IEcharts from 'vue-echarts-v3/src/lite.vue'
+// import IEcharts from 'vue-echarts-v3/src/full.vue'
+import IEcharts from 'vue-echarts-v3/src/lite.vue'
+import 'echarts/lib/chart/gauge'
 
 export default {
     name: 'hello',
     components: {
         IEcharts
     },
+    mounted () {
+        console.log(this.$refs.echart)
+    },
     data () {
         return {
             msg: 'echarts to Your Vue.js App',
-            tooltip: {
-                formatter: '{a} <br/>{b} : {c}%'
-            },
-            series: [
-                {
-                    name: '业务指标',
-                    type: 'gauge',
-                    detail: {formatter: '{value}%'},
-                    data: [{value: 50, name: '完成率'}]
-                }
-            ]
+            options: {
+                tooltip: {
+                    formatter: '{a} <br/>{b} : {c}%'
+                },
+                series: [
+                    {
+                        name: '业务指标',
+                        type: 'gauge',
+                        detail: {formatter: '{value}%'},
+                        data: [{value: 50, name: '完成率'}]
+                    }
+                ]
+            }
         }
     },
     computed: {
-        scoreOptions () {
-            console.log(this)
-            console.log(this.chartOptions)
 
-            let options = this.chartOptions
-            console.log(options)
-            options.tooltip = this.tooltip
-            options.series = this.series
-            return options
-        }
     }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
 h1, h2 {
   font-weight: normal;
 }
