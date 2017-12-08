e95a81dbbde2b8085930f84fca052addefdf99af
cag2050 <cag2050@163.com>
2017-12-08 16:09:56 +0800

    m

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
-import Echarts from '@/components/Echarts'
 
 Vue.use(Router)
 
@@ -9,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Echarts',
-      component: Echarts
+      name: 'QueryChart',
+      component: resolve => require(['../components/QueryChart'], resolve)
     },
     {
       path: '/hello',
